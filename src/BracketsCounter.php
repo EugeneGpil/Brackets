c4702954d0a51dae3725f55520ed89ae561d4192
EugeneGpil <ev@freshcube.ru>
2019-10-30 11:55:31 +0300

    BracketsCounter v1.0.1

---
+++ src/BracketsCounter.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace EugeneGpil;
+
+class BracketsCounter 
+{
+    public static function bracketsCounter($str) 
+    {
+        $acceptableSimbols = "() \n\t\r";
+    
+        $firstBracketFinded = false;
+        $bracketCounter = 0;
+    
+        $strlen = strlen($str);
+        for ($i=0; $i < $strlen; $i++) {
+    
+            if (strpos($acceptableSimbols, $str[$i]) === false) {
+                throw new InvalidArgumentException(
+                    'Invalid character "' . $str[$i] . '" in string "' . $str . '"'
+                );
+            }
+    
+            if ($str[$i] != '(' and $str[$i] != ')') {
+                continue;
+            }
+    
+            if (!$firstBracketFinded) {
+                if ($str[$i] == ')') {
+                    return false;
+                }
+                $firstBracketFinded = true;
+            }
+    
+            if ($str[$i] == '(') {
+                $bracketCounter++;
+            }
+    
+            if ($str[$i] == ')') {
+                $bracketCounter--;
+            }
+    
+            $lastBracket = $str[$i];
+        }
+    
+        if ($lastBracket == '(') {
+            return false;
+        }
+    
+        return $bracketCounter;
+    }
+}
+
